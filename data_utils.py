4c186c39033aa4150337cd36099bcb086ef860bf
Lulu Zhao <119143822+ZhaoLulu1@users.noreply.github.com>
2024-11-04 00:19:51 +0800

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -2,6 +2,7 @@ import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from dataclasses import dataclass, field
 import json
+import glob
 from typing import Any, List, Union
 from tqdm import tqdm
 
@@ -25,52 +26,6 @@ class IndexExample:
     iid: Union[int, str]
     iimage: np.ndarray
     itokens: np.ndarray
-
-# @dataclass
-# class Dataset:
-#     name: str
-#     query_examples: List[QueryExample] = field(default_factory=list)
-#     k_range: List[int] = field(default_factory=lambda: [10, 50])
-#     index_examples: List[IndexExample] = field(default_factory=list)
-
-#     def evaluate_recall(self):
-#         ret_dict = {k: [] for k in self.k_range}
-
-#         for q_example in self.query_examples:
-#             # assert len(q_example.retrieved_iids) > 0, "retrieved_iids is empty"
-#             for k in self.k_range:
-#                 recalled = False
-#                 if isinstance(q_example.target_iid, list):
-#                     for one_target_iid in q_example.target_iid:
-#                         if one_target_iid in q_example.retrieved_iids[:k]:
-#                             recalled = True
-#                 elif isinstance(q_example.target_iid, int) or isinstance(q_example.target_iid, str):
-#                     if q_example.target_iid in q_example.retrieved_iids[:k]:
-#                         recalled = True
-#                 else:
-#                     raise ValueError(f"target_iid is of type {type(q_example.target_iid)}")
-
-#                 if recalled:
-#                     ret_dict[k].append(1)
-#                 else:
-#                     ret_dict[k].append(0)
-#         total_ex = len(self.query_examples)
-#         ret_dict = {k: (sum(v) / total_ex) * 100 for k, v in ret_dict.items()}
-#         print("Recalls: ", ret_dict)
-
-#         return ret_dict
-
-#     def write_to_file(self, output_dir: str):
-#         if not os.path.exists(output_dir):
-#             os.makedirs(output_dir)
-
-#         dict_to_write = dict()
-#         for q_example in self.query_examples:
-#             dict_to_write[q_example.qid] = q_example.retrieved_iids[:50]
-#         output_file = os.path.join(output_dir, f"{self.name}_results.json")
-#         with open(output_file, "w") as f:
-#             json.dump(dict_to_write, f, indent=4)
-#         print("Results are written to file", output_file)
 
 @dataclass
 class Dataset:
@@ -327,3 +282,107 @@ def build_circo_dataset_for_train(dataset_name: str, tokenizer: Any) -> Dataset:
         print("Prepared query examples.")
 
     return train_dataset
+
+def build_happy_dataset(dataset_name: str, tokenizer: Any) -> Dataset:
+    eval_dataset = Dataset(dataset_name)
+
+    queries = []
+    for file_name in glob.glob("/home/zt/data/open-images/train/processed_nn3/*.json"):
+        with open(file_name) as f:
+            queries.extend(json.load(f))
+    index_img_ids = json.load(open(f"/home/zt/data/open-images/train/metadata"))
+    index_image_folder = "/home/zt/data/open-images/train/data"
+
+    null_tokens = tokenize("")  # used for index example
+    null_tokens = np.array(null_tokens)
+
+    def process_index_example(index_img_id):
+        img_path = os.path.join(index_image_folder, index_img_id + ".png")
+        ima = process_img(img_path, 224)
+        return IndexExample(iid=index_img_id, iimage=ima, itokens=null_tokens)
+
+    def process_query_example(query):
+        qid = query['candidate']
+        qtext = " and ".join(query['captions'])
+        qimage_path = os.path.join(index_image_folder, query['candidate'] + ".png")
+        ima = process_img(qimage_path, 224)
+        qtokens = tokenize(qtext)
+        return QueryExample(qid=qid, qtokens=qtokens, qimage=ima, target_iid=query['target'], retrieved_iids=[], retrieved_scores=[])
+
+    with ThreadPoolExecutor() as executor:
+        print("Preparing index examples...")
+        index_example_futures = {executor.submit(process_index_example, index_img_id): index_img_id for index_img_id in index_img_ids}
+
+        with tqdm(total=len(index_img_ids), desc="Index examples") as progress:
+            for future in as_completed(index_example_futures):
+                index_example = future.result()
+                eval_dataset.index_examples.append(index_example)
+                progress.update(1)
+
+        print("Prepared index examples.")
+
+        print("Preparing query examples...")
+        query_futures = {executor.submit(process_query_example, query): query for query in queries}
+
+        with tqdm(total=len(queries), desc="Query examples") as progress:
+            for future in as_completed(query_futures):
+                q_example = future.result()
+                eval_dataset.query_examples.append(q_example)
+                progress.update(1)
+        
+        print("Prepared query examples.")
+
+    return eval_dataset
+
+def build_happy_dataset_for_train(dataset_name: str, tokenizer: Any) -> Dataset:
+    train_dataset = Dataset(dataset_name)
+
+    queries = []
+    for file_name in glob.glob("/home/zt/data/open-images/train/processed_nn1/*.json") + glob.glob("/home/zt/data/open-images/train/processed_nn2/*.json"):
+        with open(file_name) as f:
+            queries.extend(json.load(f))
+    index_img_ids = json.load(open(f"/home/zt/data/open-images/train/metadata"))
+    index_image_folder = "/home/zt/data/open-images/train/data"
+
+    null_tokens = tokenize("")  # used for index example
+    null_tokens = np.array(null_tokens)
+
+    def process_index_example(index_img_id):
+        img_path = os.path.join(index_image_folder, index_img_id + ".jpg")
+        ima = process_img(img_path, 224)
+        return IndexExample(iid=index_img_id, iimage=ima, itokens=null_tokens)
+
+    def process_query_example(query):
+        qid = query['candidate']
+        qtext = " and ".join(query['captions'])
+        qimage_path = os.path.join(index_image_folder, query['candidate'] + ".png")
+        ima = process_img(qimage_path, 224)
+        qtokens = tokenize(qtext)
+        return QueryExample(qid=qid, qtokens=qtokens, qimage=ima, target_iid=query['target'], retrieved_iids=[], retrieved_scores=[])
+
+    with ThreadPoolExecutor() as executor:
+        print("Preparing index examples...")
+        index_example_futures = {executor.submit(process_index_example, index_img_id): index_img_id for index_img_id in index_img_ids}
+
+        with tqdm(total=len(index_img_ids), desc="Index examples") as progress:
+            for future in as_completed(index_example_futures):
+                index_example = future.result()
+                train_dataset.index_examples.append(index_example)
+                progress.update(1)
+
+        print("Prepared index examples.")
+
+        print("Preparing query examples...")
+        query_futures = {executor.submit(process_query_example, query): query for query in queries}
+
+        with tqdm(total=len(queries), desc="Query examples") as progress:
+            for future in as_completed(query_futures):
+                q_example = future.result()
+                train_dataset.query_examples.append(q_example)
+                progress.update(1)
+            
+        print("Prepared query examples.")
+
+
+    return train_dataset
+
